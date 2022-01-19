3e354af75465bd83f1def2e7a6433770b6b9ad55
nina-osk <ing.oscarina@gmail.com>
2022-01-18 21:05:20 -0300

    commit final

---
+++ App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import { StatusBar } from 'expo-status-bar';
+ import React from 'react';
+ import { StyleSheet, View } from 'react-native';
+ import CarsList from './components/CarsList';
+ import Header from './components/Header';
+ 
+ export default function App() {
+   return (
+     <View style={styles.container}>
+       <Header />
+       <CarsList />
+       <StatusBar style="auto" />
+     </View>
+   );
+ }
+ 
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     backgroundColor: '#fff',
+     alignItems: 'center',
+     justifyContent: 'center',
+   },
+ });
